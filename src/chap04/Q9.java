4370d7c7ed9c5eeab4bc14a80d670436ffc4479c
wzhishen <wzhishen@gmail.com>
2015-08-27 14:53:35 -0400

    Fixed bug in chap04 Q9.

--- src/chap04/Q9.java
+++ src/chap04/Q9.java
@@ -1,11 +1,11 @@
 package chap04;
 
 public class Q9 {
-    static //You are given a binary tree in which each node contains a value. Design an algorithm
+    //You are given a binary tree in which each node contains a value. Design an algorithm
     //to print all paths which sum to a given value. The path does not need to
     //start or end at the root or a leaf.
     
-    void findSum(TreeNode root, int sum) {
+    static void findSum(TreeNode root, int sum) {
         findSum(root, sum, new int[getHeight(root)], 0);
     }
     
@@ -15,7 +15,7 @@ public class Q9 {
         path[level] = n.value;
         int s = 0;
         for (int i = level; i >= 0; --i) {
-            s += path[level];
+            s += path[i];
             if (s == sum)
                 print(path, i, level);
         }

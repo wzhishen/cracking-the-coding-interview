1cd0a051cee0e5037c372cc3e8fb422839605308
wzhishen <wzhishen@gmail.com>
2015-10-14 22:20:41 -0400

    Removed duplicate files and updated Q6 chap04, Q3 chap05.

--- src/chap04/Q6.java
+++ src/chap04/Q6.java
@@ -5,16 +5,16 @@ import static helpers.Printer.*;
 import helpers.TreeNode;
 
 /**
- * Write an algorithm to find the'next'node (i.e., in-order successor)
- * of a given node in a binary search tree. You may assume that each
- * node has a link to its parent.
+ * Write an algorithm to find the 'next' node (i.e., in-order successor)
+ * of a given node in a binary search tree. You may assume that each node
+ * has a link to its parent.
  */
 public class Q6 {
     public static TreeNode inorderSuccessor(TreeNode n) {
         if (n == null) return null;
 
         // case 1: n has right subtree ->
-        //         just return leftmost node of right subtree
+        //         return leftmost node of right subtree
         if (n.right != null) return leftmostChild(n.right);
 
         // case 2:   n has no right subtree
@@ -36,6 +36,49 @@ public class Q6 {
         return leftmostChild(n.left);
     }
 
+    public static TreeNode preorderSuccessor(TreeNode n) {
+        if (n == null) return null;
+
+        // case 1: n has a child ->
+        //         just return that child (left if exists, otherwise right)
+        if (n.left != null) return n.left;
+        else if (n.right != null) return n.right;
+        // case 2: n has no child ->
+        //         n climbs up until reaching a parent that has a right child
+        //         (which is not n), then return this right child
+        while (n.parent != null && (n.parent.right == null || n.parent.right == n)) {
+            n = n.parent;
+        }
+        // case 3: n is the last node in traversal ->
+        //         return root's parent, ie., null
+        if (n.parent == null) return null;
+        return n.parent.right;
+    }
+
+    public static TreeNode postorderSuccessor(TreeNode n) {
+        // case 1: n is the last node in traversal ->
+        //         return root's parent, ie., null
+        if (n == null || n.parent == null) return null;
+        // case 2:   n is left child of its parent ->
+        //           just return parent
+        // case 3:   n is right child of its parent
+        // case 3.1: parent has no right child ->
+        //           just return parent
+        if (n.parent.right == n || n.parent.right == null) return n.parent;
+        // case 3.2: parent has right child ->
+        //           return leftmost bottom node of parent's right subtree
+        return leftmostBottomChild(n.parent.right);
+    }
+
+    private static TreeNode leftmostBottomChild(TreeNode n) {
+        if (n.left == null && n.right == null) return n;
+        if (n.left != null) {
+            return leftmostBottomChild(n.left);
+        } else {
+            return leftmostBottomChild(n.right);
+        }
+    }
+
     //TEST----------------------------------
     public static void main(String[] args) {
         /*
@@ -51,10 +94,23 @@ public class Q6 {
         n4.left = n2; n4.right = n5; n2.parent = n4; n5.parent = n4;
         TreeNode.printTree(n4);
 
-        TreeNode n = n1;
+        TreeNode n = n4;
+        print("pre-order:  ");
+        while (n != null) {
+            print(n + " ");
+            n = preorderSuccessor(n);
+        }
+        print("\nin-order:   ");
+        n = n1;
         while (n != null) {
             print(n + " ");
             n = inorderSuccessor(n);
+        }
+        print("\npost-order: ");
+        n = n1;
+        while (n != null) {
+            print(n + " ");
+            n = postorderSuccessor(n);
         }
     }
 }
--- src/chap05/Q3.java
+++ src/chap05/Q3.java
@@ -1,41 +1,53 @@
 package chap05;
 
+import static helpers.Printer.*;
+import static helpers.Bit.*;
+
+/**
+ * Given a positive integer, print the next smallest and the next
+ * largest number that have the same number of 1 bits in their
+ * binary representation.
+ */
 public class Q3 {
-//    Given a positive integer, print the next smallest and the next largest number that
-//    have the same number of 1 bits in their binary representation.
-    
-    static int getNext(int n) {
+    public static int getNext(int n) {
         assert n > 0;
         int numOnes = countNumOnes(n);
-        for (int i = n+1; i < Integer.MAX_VALUE;++i) {
+        for (int i = n + 1; i <= Integer.MAX_VALUE; ++i) {
             if (countNumOnes(i) == numOnes)
                 return i;
         }
         return -1;
     }
-    
-    static int getPrev(int n) {
+
+    public static int getPrev(int n) {
         assert n > 0;
         int numOnes = countNumOnes(n);
-        for (int i = n-1; i > 0; --i) {
+        for (int i = n - 1; i > 0; --i) {
             if (countNumOnes(i) == numOnes)
                 return i;
         }
         return -1;
     }
-    
+
     private static int countNumOnes(int n) {
         int cnt = 0;
-        while (n > 0) {
-            if (n % 2 == 1) ++cnt;
-            n /= 2;
+        for (int i = n; i > 0; i &= (i - 1)) {
+            ++cnt;
         }
         return cnt;
     }
-    
-    //--------------------------------------------------
+
+    //TEST----------------------------------
     public static void main(String[]args) {
-        System.out.println(getNext(5)+" "+getPrev(5));
+        test(5);
+        test(50);
     }
 
+    private static void test(int n) {
+        println(n + " (" + toBitString(n) + ")");
+        int next = getNext(n), prev = getPrev(n);
+        println("next: " + next + " ("+ toBitString(next) + ")");
+        println("prev: " + prev + " ("+ toBitString(prev) + ")");
+        println();
+    }
 }

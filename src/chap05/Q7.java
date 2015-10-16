0fe42576e6d6be675d23d0ed9c58b037c124b62b
wzhishen <wzhishen@gmail.com>
2015-10-16 14:54:34 -0400

    Updated Q7-Q8 chap05.

--- src/chap05/Q7.java
+++ src/chap05/Q7.java
@@ -1,38 +1,74 @@
 package chap05;
 
+import static helpers.Printer.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * An array A contains all the integers from 0 to n, except for
+ * one number which is missing. In this problem, we cannot access
+ * an entire integer in A with a single operation. The elements
+ * of A are represented in binary, and the only operation we can
+ * use to access them is "fetch the j-th bit of A[i]," which takes
+ * constant time. Write code to find the missing integer. Can you
+ * do it in 0(n) time?
+ */
 public class Q7 {
-//    An array A contains all the integers from 0 to n, except for one number which is
-//    missing. In this problem, we cannot access an entire integer in A with a single
-//    operation. The elements of A are represented in binary, and the only operation
-//    we can use to access them is "fetch the jth bit of A[i]," which takes constant
-//    time. Write code to find the missing integer. Can you do it in 0(n) time?
-    
-    int findMissingInt(int[] a, int n) {
-        int sum = 0;
-        for (int i = 0; i < a.length; ++i) {
-            int base = 1;
-            int num = 0;
-            for (int j = 0; j <= length(a[i]); ++j) {
-                num += fetch(a, i, j) * base;
-                base *= 2;
-            }
-            sum += num;
+    /* SOLUTION
+     * 1. Start with the first least significant bit (LSB(0)).
+     * 2. Count occurrences of 1's vs 0's.
+     * 3. If count(1) < count(0), then the missing number has a 1 as its LSB,
+     *    else it has a 0.
+     * 4. Only retain all numbers with LSB matching result found in step 3.
+     * 5. Repeat steps 1-4, iterating from least significant bit (LSB(0))
+     *    -> 2nd least significant bit (LSB(1)) -> ...
+     *    -> most significant bit (LSB(N))
+     */
+    public static int findMissingInteger(ArrayList<Integer> a) {
+        int j = 0;
+        int n = 0;
+        while (a.size() > 0) {
+            int bit = findMissingIntegerBit(a, j);
+            n |= bit << j;
+            a = filter(a, j, bit);
+            ++j;
         }
-        return sum - (0 + n)*n/2;
-    }
-    
-    private int fetch(int[] a, int i, int j) {
-        /* fetch the jth bit of A[i]
-         * Not implemented
-         */
-        return 0;
-    }
-    
-    private int length(int bin) {
-        /* get length of binary
-         * Not implemented
-         */
-        return 0;
+        return n;
     }
 
+    private static int findMissingIntegerBit(ArrayList<Integer> a, int j) {
+        int numOnes = 0;
+        int numZeros = 0;
+        for (int i = 0; i < a.size(); ++i) {
+            int bit = fetch(a, i, j);
+            if (bit == 1) ++numOnes;
+            else ++numZeros;
+        }
+        if (numOnes < numZeros) return 1;
+        else return 0;
+    }
+
+    private static ArrayList<Integer> filter(ArrayList<Integer> a, int j, int bit) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < a.size(); ++i) {
+            if (fetch(a, i, j) == bit)
+                result.add(a.get(i));
+        }
+        return result;
+    }
+
+    private static int fetch(ArrayList<Integer> a, int i, int j) {
+        int n = a.get(i);
+        return (n >> j) & 1;
+    }
+
+    //TEST----------------------------------
+    public static void main(String[]args) {
+        Integer[] a = {0,1,2,3,5,6,7,8,9,10};
+        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(a));
+        Collections.shuffle(list);
+        println(findMissingInteger(list));
+    }
 }
--- src/chap05/Q8.java
+++ src/chap05/Q8.java
@@ -1,38 +1,65 @@
 package chap05;
 
+import static helpers.Printer.*;
+
+/**
+ * A monochrome screen is stored as a single array of bytes, allowing eight consecutive pixels
+ * to be stored in one byte.The screen has width w, where w is divisible by 8 (that is, no byte
+ * will be split across rows). The height of the screen, of course, can be derived from the length
+ * of the array and the width. Implement a function drawHorizontalLine(byte[] screen, int width,
+ * int x1, int x2, int y) which draws a horizontal line from (x1, y) to (x2, y).
+ */
 public class Q8 {
-//    A monochrome screen is stored as a single array of bytes, allowing eight consecutive
-//    pixels to be stored in one byte.The screen has width w, where w is divisible
-//    by 8 (that is, no byte will be split across rows).The height of the screen, of course,
-//    can be derived from the length of the array and the width. Implement a function
-//    drawHorizontall_ine(byte[] screen, int width, int xl, int x2,
-//    int y) which draws a horizontal line from (xl, y)to(x2, y).
-    
-    void drawLine(byte[] screen, int width, int x1, int x2, int y) {
+    public static void drawLine(byte[] screen, int width, int x1, int x2, int y) {
         int startOffset = x1 % 8;
         int firstByte = startOffset == 0 ? x1/8 : x1/8 + 1;
-        int endOffset = (x2+1) % 8;
-        int lastByte = endOffset == 0 ? x2/8 : x2/8 - 1;
-        
+        int endOffset = x2 % 8;
+        int lastByte = endOffset == 7 ? x2/8 : x2/8 - 1;
+
         // draw line for full bytes
         for (int i = firstByte; i <= lastByte; ++i) {
             screen[i + width/8*y] = (byte) 0xFF;
         }
-        
-        // draw start/end of line
+
+        // draw start and end of line
+        byte startMask = (byte) (0xFF >> startOffset);
+        byte endMask = (byte) ~(0xFF >> (endOffset + 1));
         if (x1/8 == x2/8) {
-            byte mask = (byte) (0xFF >> startOffset);
-            mask &= (0xFF << 8-endOffset);
-            screen[firstByte - 1 + width/8*y] |= mask;
+            byte mask = (byte) (startMask & endMask);
+            screen[x1/8 + width/8*y] |= mask;
         }
         else {
             if (startOffset != 0) {
-                screen[firstByte - 1 + width/8*y] |= (byte)(0xFF >> startOffset);
+                screen[firstByte - 1 + width/8*y] |= startMask;
             }
-            if (endOffset != 0) {
-                screen[lastByte + 1 + width/8*y] |= (byte)(0xFF << 8-endOffset);
+            if (endOffset != 7) {
+                screen[lastByte + 1 + width/8*y] |= endMask;
             }
         }
     }
 
+    //TEST----------------------------------
+    public static void main(String[]args) {
+        byte[] screen = new byte[16];
+        int width = 32;
+        printScreen(screen, width);
+        drawLine(screen, width, 0, 6, 0);
+        printScreen(screen, width);
+        drawLine(screen, width, 31,31, 1);
+        drawLine(screen, width, 2, 29, 2);
+        drawLine(screen, width, 1, 5, 3);
+        printScreen(screen, width);
+    }
+
+    private static void printScreen(byte[] screen, int width) {
+        int num = 1, widthNum = width/8;
+        for (byte b : screen) {
+            for (int i = 7; i >= 0; --i) {
+                print((b >> i) & 1);
+            }
+            if (num % widthNum == 0) println();
+            ++num;
+        }
+        println();
+    }
 }
